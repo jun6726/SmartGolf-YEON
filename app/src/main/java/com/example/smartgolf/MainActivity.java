95977b4edf20333a28dee5a5280364e2158a31a6
YEON <junmarko@naver.com>
2019-08-20 14:53:38 +0900

    코드 정리 및 동적 그래프 수정

--- app/src/main/java/com/example/smartgolf/MainActivity.java
+++ app/src/main/java/com/example/smartgolf/MainActivity.java
@@ -16,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import android.widget.Toast;
 
@@ -28,28 +29,43 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
+class MyVector{
+    public float x,y,z;
+
+    MyVector(){
+        this.x = 0.f;
+        this.y = 0.f;
+        this.z = 0.f;
+    }
+    public MyVector(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    ArrayList<MyVector> MyVector;
 
     private SensorManager sensorManager;
     private Sensor accelerSensor;
     private Sensor linearAccelerSensor;
-    private float gAccX, gAccY, gAccZ, accX, accY, accZ, lAccX, lAccY, lAccZ;
-    private float tempX, tempY, tempZ;
-    private double gTotal, total, lTotal;
-    private float alpha = 0.8f;
-    TextView tvgXaxis, tvgYaxis, tvgZaxis, tvXaxis, tvYaxis, tvZaxis, tvlXaxis, tvlYaxis, tvlZaxis, tvgTotal, tvTotal, tvlTotal;
+    public float lAccX, lAccY, lAccZ;
+    TextView tvlXaxis, tvlYaxis, tvlZaxis, tvlTotal;
     Button button;
 
     private LineChart lineChart;
 
     int count;
-    private int time = 5;
-    private float dt = 0.5f;   // 임시
+    private int time = 5000;
+    private float dt = 0.1f;   // 임시
     int stepnum = (int)(time / dt);
-//    float[][] Time, Acc, Vel, Loc;
-    float[][] Acc;
+    float five_second;
+
+    float[][] Acc = new float[stepnum][3];
     float[][] Vel = new float[stepnum][3];
-    float[][] Dis = new float[stepnum][3];
+    float[][] Loc = new float[stepnum][3];
 
     float start_time, end_time;
 
@@ -58,18 +74,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        tvgXaxis = (TextView) findViewById(R.id.tvgXaxis);
-//        tvgYaxis = (TextView) findViewById(R.id.tvgYaxis);
-//        tvgZaxis = (TextView) findViewById(R.id.tvgZaxis);
-//        tvXaxis = (TextView) findViewById(R.id.tvXaxis);
-//        tvYaxis = (TextView) findViewById(R.id.tvYaxis);
-//        tvZaxis = (TextView) findViewById(R.id.tvZaxis);
         tvlXaxis = (TextView) findViewById(R.id.tvlXaxis);
         tvlYaxis = (TextView) findViewById(R.id.tvlYaxis);
         tvlZaxis = (TextView) findViewById(R.id.tvlZaxis);
-//        tvgTotal = (TextView) findViewById(R.id.tvgTotal);
-//        tvTotal = (TextView) findViewById(R.id.tvTotal);
-        tvlTotal = (TextView) findViewById(R.id.tvlTotal);
         button = (Button) findViewById(R.id.button);
         lineChart = (LineChart) findViewById(R.id.chart);
 
@@ -78,15 +85,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         linearAccelerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 
         // 기능
-        MakeChart(1, 1);
+        MakeChart(0, 0);
 
         count = 0;
         button.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if (motionEvent.getAction() == motionEvent.ACTION_DOWN) {
-//                    start_time = System.currentTimeMillis();
-////                    end_time = start_time + 5000;
+                    start_time = System.currentTimeMillis()  / 1000;
+                    end_time = start_time + 5000;
+                    five_second = end_time - start_time;
+                    Toast.makeText(MainActivity.this, "시간차"+five_second, Toast.LENGTH_SHORT).show();
                 }
                 if (motionEvent.getAction() == motionEvent.ACTION_UP) {
                 }
@@ -110,35 +119,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor == accelerSensor) {
-            //include gravity
-            gAccX = event.values[0];
-            gAccY = event.values[1];
-            gAccZ = event.values[2];
-
-//            tempX = alpha * tempX + (1 - alpha) * event.values[0];
-//            tempY = alpha * tempY + (1 - alpha) * event.values[1];
-//            tempZ = alpha * tempZ + (1 - alpha) * event.values[2];
-//
-//
-//            accX = event.values[0] - tempX;
-//            accY = event.values[1] - tempY;
-//            accZ = event.values[2] - tempZ;
-//
-//            gTotal = Math.sqrt(Math.pow(gAccX, 2) + Math.pow(gAccY, 2) + Math.pow(gAccZ, 2));
-//            total = Math.sqrt(Math.pow(accX, 2) + Math.pow(accY, 2) + Math.pow(accZ, 2));
-//
-//            tvgXaxis.setText("중력 가속도 X axis : " + String.format("%.2f", gAccX));
-//            tvgYaxis.setText("중력 가속도 Y axis : " + String.format("%.2f", gAccY));
-//            tvgZaxis.setText("중력 가속도 Z axis : " + String.format("%.2f", gAccZ));
-//            tvgTotal.setText("Total Gravity : " + String.format("%.2f", gTotal) + " m/s\u00B2");
-//
-//            tvXaxis.setText("X axis : " + String.format("%.2f", accX));
-//            tvYaxis.setText("Y axis : " + String.format("%.2f", accY));
-//            tvZaxis.setText("Z axis : " + String.format("%.2f", accZ));
-//            tvTotal.setText("Total Gravity : " + String.format("%.2f", total) + " m/s\u00B2");
-        }
-
         // 가속도 파트 , Acc
         if (event.sensor == linearAccelerSensor) {
             //exclude gravity
@@ -146,22 +126,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             lAccY = event.values[1];
             lAccZ = event.values[2];
 
-            lTotal = Math.sqrt(Math.pow(lAccX, 2) + Math.pow(lAccY, 2) + Math.pow(lAccZ, 2));
-
             tvlXaxis.setText("선형 X axis : " + String.format("%.2f", lAccX));
             tvlYaxis.setText("선형 Y axis : " + String.format("%.2f", lAccY));
             tvlZaxis.setText("선형 Z axis : " + String.format("%.2f", lAccZ));
-            tvlTotal.setText("Total Gravity : " + String.format("%.2f", lTotal) + " m/s\u00B2");
 
-            Acc = new float[5000][3];
-            Acc[count][0] = lAccX;
-            Acc[count][1] = lAccY;
-            Acc[count][2] = lAccZ;
+            Acc[count] = new float[]{lAccX, lAccY, lAccZ};
 
-            updateMarker(count, lAccX);
+            if(count>1) {
+                Vel = Numeric_Integration(Acc);
+
+            }
+            if(count>2) {
+                Loc = Numeric_Integration(Vel);
+                updateMarker(count, Loc[count][1]);
+            }
             count++;
-//            Log.d("초당 센서 갯수 확인용", ""+lAccX);
-            Log.d("Acc배열 확인","count"+count+"  Acc[count]"+Acc[count]+"  Acc[count][0]"+Acc[count][0]+ "   lAccx"+lAccX);
         }
     }
 
@@ -174,8 +153,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     public void MakeChart(float xValue, float yValue) {
         entries.add(new Entry(xValue, yValue));
-        entries.add(new Entry(xValue + 1, yValue + 1));
-        entries.add(new Entry(xValue + 2, yValue + 2));
 
         LineDataSet lineDataSet;
         lineDataSet = new LineDataSet(entries, "속성명1");
@@ -212,7 +189,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         lineChart.setDoubleTapToZoomEnabled(false);
         lineChart.setDrawGridBackground(false);
         lineChart.setDescription(description);
-//        lineChart.animateY(2000, Easing.EasingOption.EaseInCubic);
         MyMarkerView marker = new MyMarkerView(this, R.layout.markerview);
         marker.setChartView(lineChart);
         lineChart.setMarker(marker);
@@ -225,7 +201,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         lineData.notifyDataChanged();
 
         lineChart.notifyDataSetChanged();
-        lineChart.setVisibleXRangeMaximum(120);
         lineChart.moveViewToX(lineData.getEntryCount());
 
         MyMarkerView marker = new MyMarkerView(this, R.layout.markerview);
@@ -233,10 +208,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         lineChart.setMarker(marker);
     }
 
-    // 가속도 -> 속도 -> 위치
-    // v1 = (a0 + a1)(t1-t0) / 2
-    private float Numerical_Integration(float val0, float val1, float time0, float time1) {
-            float transform_V =  ((val0 + val1)*(time1 - time0) / 2);
-        return transform_V;
+    private float[][] Numeric_Integration(float[][] paraArray){
+        float[][] Temp = new float[stepnum][3];
+
+        // temp 초기화 첫행은 속도와 위치 모두 0
+        for(int i = 0 ; i<3 ; i++){
+            Temp[0][i] = 0;
+        }
+
+        for(int j = 1 ; j < stepnum ; j++) {
+            for (int k = 0; k < 3; k++) {
+                Temp[j][k] = Temp[j - 1][k] + ((paraArray[j][k] + paraArray[j - 1][k]) * dt / 2);
+            }
+        }
+        return Temp;
     }
 }
