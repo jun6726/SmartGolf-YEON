a80efcf3dbb74442367ab44ac1edb5e75d4184d8
YEON <junmarko@naver.com>
2019-08-21 14:53:55 +0900

    커밋내용 : 파일저장 경로 변경 완료, csv파일에 x,y,z값 저장,            스윙 시작 버튼 클릭부터 그래프 활성화 버튼 재 클릭 시 정지. 진행방향 : 그래프 초기화 진행 중

--- app/src/main/java/com/example/smartgolf/MainActivity.java
+++ app/src/main/java/com/example/smartgolf/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.smartgolf;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -22,6 +23,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
@@ -52,9 +54,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor linearAccelerSensor;
     public float lAccX, lAccY, lAccZ;
     TextView tvlXaxis, tvlYaxis, tvlZaxis, tvlTotal;
-    Button button, btn_capture, btn_excel;
+    Button button, btn_capture, btn_excel,btn_reset;
 
     private LineChart lineChart;
+    LineDataSet lineDataSet;
+    LineData lineData;
 
     int count;
     private int time = 5000;
@@ -65,13 +69,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     float[][] Vel = new float[stepnum][3];
     float[][] Loc = new float[stepnum][3];
 
-    String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
-    String fileName = "SwingData.csv";
-    String filePath = baseDir + File.separator + fileName;
-    File file = new File(filePath);
+    boolean isBtnOn = false;
+    float FileSaveTime = System.currentTimeMillis()*1000;
+    int FileSaveTime_ver = 0;
 
+    File FilePath = new File(Environment.getExternalStorageDirectory() + "/Download");
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -81,6 +87,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         button = (Button) findViewById(R.id.button);
         btn_capture = (Button) findViewById(R.id.btn_capture);
         btn_excel = (Button) findViewById(R.id.btn_excel);
+        btn_reset = (Button) findViewById(R.id.btn_reset);
         lineChart = (LineChart) findViewById(R.id.chart);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -88,20 +95,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         linearAccelerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 
         checkPermission();
-        // 기능
-        MakeChart(0, 0);
+        MakeChart(0,0);
 
         count = 0;
-        button.setOnTouchListener(new View.OnTouchListener() {
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == motionEvent.ACTION_DOWN) {
-                    onResume();
-                }
-                if (motionEvent.getAction() == motionEvent.ACTION_UP) {
+            public void onClick(View view) {
+                if(isBtnOn == true) {
+                    isBtnOn = false;
                     onPause();
                 }
-                return false;
+                else if(isBtnOn == false) {
+                    isBtnOn = true;
+                    onResume();
+                }
             }
         });
 
@@ -124,17 +132,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 saveExcel();
             }
         });
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lineChart.clearValues();
+                MakeChart(0,0);
+            }
+        });
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         sensorManager.registerListener(this, accelerSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, linearAccelerSensor, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(this, linearAccelerSensor, SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
-    protected void onPause() {
+    public void onPause() {
         super.onPause();
         sensorManager.unregisterListener(this);
     }
@@ -142,25 +158,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
         // 가속도 파트 , Acc
-        if (event.sensor == linearAccelerSensor) {
+        if (event.sensor == linearAccelerSensor && isBtnOn == true) {
             //exclude gravity
             lAccX = event.values[0];
             lAccY = event.values[1];
             lAccZ = event.values[2];
 
-            Log.d("lAccX", ""+lAccX);
+            Log.d("lAccX", "" + lAccX);
             tvlXaxis.setText("선형 X axis : " + String.format("%f", lAccX));
             tvlYaxis.setText("선형 Y axis : " + String.format("%f", lAccY));
             tvlZaxis.setText("선형 Z axis : " + String.format("%f", lAccZ));
 
             Acc[count] = new float[]{lAccX, lAccY, lAccZ};
 
-            if (count > 2) {
-//                Vel = Numeric_Integration(Acc);
-//                Loc = Numeric_Integration(Vel);
-//                updateMarker(count, Vel[count][1]);
-//                updateMarker(count, Loc[count][1]);
-            }
             count++;
             updateMarker(count, lAccX);
         }
@@ -171,25 +181,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
+
     List<Entry> entries = new ArrayList<>();
 
     public void MakeChart(float xValue, float yValue) {
         entries.add(new Entry(xValue, yValue));
 
-        LineDataSet lineDataSet;
         lineDataSet = new LineDataSet(entries, "속성명1");
         lineDataSet.setLineWidth(2);
         lineDataSet.setCircleRadius(6);
-        lineDataSet.setCircleColor(Color.BLACK);
-        lineDataSet.setCircleColorHole(Color.YELLOW);
-        lineDataSet.setColor(Color.BLACK);
+        lineDataSet.setCircleColor(Color.BLUE);
+        lineDataSet.setCircleColorHole(Color.BLUE);
+        lineDataSet.setColor(Color.BLUE);
         lineDataSet.setDrawCircleHole(true);
         lineDataSet.setDrawCircles(true);
         lineDataSet.setDrawHorizontalHighlightIndicator(false);
         lineDataSet.setDrawHighlightIndicators(false);
         lineDataSet.setDrawValues(true);
 
-        LineData lineData = new LineData(lineDataSet);
+        lineData = new LineData(lineDataSet);
         lineChart.setData(lineData);
 
         XAxis xAxis = lineChart.getXAxis();
@@ -230,21 +240,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     private void checkPermission() {
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // 마시멜로우 버전과 같거나 이상이라면
             if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                     || checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 if(shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                    Toast.makeText(this, "외부 저장소 사용을 위해 읽기/쓰기 필요", Toast.LENGTH_SHORT).show();
-                }
-
-                requestPermissions(new String[]
-                                {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
-                        2);  //마지막 인자는 체크해야될 권한 갯수
-
-            } else {
-                //Toast.makeText(this, "권한 승인되었음", Toast.LENGTH_SHORT).show();
-            }
+                    Toast.makeText(this, "외부 저장소 사용을 위해 읽기/쓰기 필요", Toast.LENGTH_SHORT).show(); }
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 2);
+                //마지막 인자는 체크해야될 권한 갯수
+            } else { Toast.makeText(this, "권한 승인되었음", Toast.LENGTH_SHORT).show(); }
         }
     }
 
@@ -258,52 +261,50 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         cell = row.createCell(0); // 1번 셀 생성
         cell.setCellValue("AccX"); // 1번 셀 값 입력
-
         cell = row.createCell(1); // 2번 셀 생성
         cell.setCellValue("AccY"); // 2번 셀 값 입력
-
         cell = row.createCell(2); // 3번 셀 생성
         cell.setCellValue("AccZ"); // 3번 셀 값 입력
 
-        for(int i = 0; i < 20 ; i++){ // 데이터 엑셀에 입력
-            row = sheet.createRow(i+1);
+        for(int i = 0; i < count ; i++){ // 데이터 엑셀에 입력
+            row = sheet.createRow(i + 1);
             cell = row.createCell(0);
-//            cell.setCellValue(mItems.get(i).getName());
+            cell.setCellValue(Acc[i][0]);
             cell = row.createCell(1);
-//            cell.setCellValue(mItems.get(i).getAge());
+            cell.setCellValue(Acc[i][1]);
+            cell = row.createCell(2);
+            cell.setCellValue(Acc[i][2]);
         }
 
-        File xlsFile = new File(Environment.getExternalStorageDirectory(), "test.csv");
-        Log.e("file path", String.valueOf(xlsFile));
+        File csvFile = new File(FilePath, FileSaveTime+"_"+FileSaveTime_ver+"_csv.csv");
         try{
-            FileOutputStream os = new FileOutputStream(xlsFile);
+            FileOutputStream os = new FileOutputStream(csvFile);
             workbook.write(os); // 외부 저장소에 엑셀 파일 생성
+            FileSaveTime_ver++;
         }catch (IOException e){
             e.printStackTrace();
         }
-        Toast.makeText(getApplicationContext(),xlsFile.getAbsolutePath()+"에 저장되었습니다", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),csvFile.getAbsolutePath()+"에 저장되었습니다", Toast.LENGTH_SHORT).show();
     }
 
     //화면 캡쳐하기
     public File ScreenShot(View view){
         view.setDrawingCacheEnabled(true);  //화면에 뿌릴때 캐시를 사용하게 한다
-
         Bitmap screenBitmap = view.getDrawingCache();   //캐시를 비트맵으로 변환
-
-        String filename = "screenshot.png";
-        File file = new File(Environment.getExternalStorageDirectory()+"/img.jpg");  //Pictures폴더 screenshot.png 파일
+        File file = new File(FilePath,FileSaveTime+"_"+FileSaveTime_ver+"_img.jpg");
         FileOutputStream os = null;
         try{
             os = new FileOutputStream(file);
             screenBitmap.compress(Bitmap.CompressFormat.PNG, 90, os);   //비트맵을 PNG파일로 변환
             os.close();
+            FileSaveTime_ver++;
             Toast.makeText(MainActivity.this, "이미지 캡쳐", Toast.LENGTH_SHORT).show();
+            lineChart.invalidate();
         }catch (IOException e){
             e.printStackTrace();
             Log.e("IOException", ""+e);
             return null;
         }
-
         view.setDrawingCacheEnabled(false);
         return file;
     }
