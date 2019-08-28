9c219a03414a13e6f37ae86d70c5a2bc4131b25f
YEON <junmarko@naver.com>
2019-08-28 20:04:20 +0900

    커밋내용 : 재 학습된 모델 적용, glide 라이브러리로 다이어로그에 애니메이션 (gif)사용 문제점 : 텐서플로우 버튼 입력시 텐서플로우로 들어가는게 아니라 센서가 변경되고 있으며 150시퀀스마다 텐서플로우 실행
    진행방향 : 텐서플로우 버튼 입력시 텐서플로우 실행되도록, 코드 정리(불필요한 코드 제거), 예외 처리

--- app/src/main/java/com/example/smartgolf/MainActivity.java
+++ app/src/main/java/com/example/smartgolf/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.smartgolf;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.hardware.Sensor;
@@ -18,9 +20,11 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
@@ -31,6 +35,9 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.Resource;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
@@ -39,131 +46,95 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 
+import org.apache.poi.ss.formula.functions.T;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
-    private Sensor accelerSensor;
-    private Sensor linearAccelerSensor;
-    private Sensor gyroSensor;
+    private Sensor accelerSensor, linearAccelerSensor, gyroSensor;
     public float lAccX, lAccY, lAccZ;
     public float GyroX, GyroY, GyroZ;
-    TextView tvlXaxis, tvlYaxis, tvlZaxis, tvlTotal;
-    Button button, btn_capture, btn_excel, btn_reset, btn_tensor;
+    Button btn_swing, btn_excel, btn_reset, btn_tensor, btn_Force;
+    TextView textView;
 
     private LineChart lineChart;
     LineDataSet lineDataSet;
     LineData lineData;
 
-    int count;
+    public int count;
     private int time = 5000;
 
-    float[][] lAcc = new float[time][3];
     float[][] Gyro = new float[time][3];
     float[][] GGap = new float[time][3];
     double[][] Pos = new double[time][3];
     double[][] Torque = new double[time][2];
+    double[][] Force = new double[time][2];
+
     float[] SensorTime = new float[time];
 
     boolean isBtnOn = false;
     float FileSaveTime = System.currentTimeMillis() * 1000;
 
-    float dt;
-
     int FileSaveTime_ver = 0;
 
-    float pitch;
-    float roll;
-    float yaw;
+    float dt, pitch, roll, yaw, radios;
 
-    float radios;
-    double theta;
-    double pie;
-
+    double totalRadios;
+    double theta, pie;
     double Moment_Inertia;
-
-    double AngAccX;
-    double AngAccY;
-    double AngAccZ;
-
-    double theta_Acc;
-    double pie_Acc;
+    double AngAccX, AngAccY, AngAccZ;
+    double theta_Acc, pie_Acc;
 
     File FilePath = new File(Environment.getExternalStorageDirectory() + "/Download");
 
     float[][][] inputGyro = new float[2][140][3];
     float[] outGyro = new float[]{};
 
-    //테스트
-    private static final int N_SAMPLES = 200;
-    private static List<Float> x;
-    private static List<Float> y;
-    private static List<Float> z;
+    private static List<Float> x,y,z;
 
     private float[] results;
     public TensorFlowClassifier classifier;
 
+    public Boolean P_S;
+//    public DataFilter_Cal dataFilter_cal;
 
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        classifier = new TensorFlowClassifier(getApplicationContext());
-        //테스트
-        x = new ArrayList<>();
-        y = new ArrayList<>();
-        z = new ArrayList<>();
-
-        tvlXaxis = (TextView) findViewById(R.id.tvlXaxis);
-        tvlYaxis = (TextView) findViewById(R.id.tvlYaxis);
-        tvlZaxis = (TextView) findViewById(R.id.tvlZaxis);
-        button = (Button) findViewById(R.id.button);
-        btn_capture = (Button) findViewById(R.id.btn_capture);
+        btn_swing = (Button) findViewById(R.id.btn_swing);
         btn_excel = (Button) findViewById(R.id.btn_excel);
         btn_reset = (Button) findViewById(R.id.btn_reset);
         btn_tensor = (Button) findViewById(R.id.btn_tensor);
         lineChart = (LineChart) findViewById(R.id.chart);
-
+        btn_Force = (Button) findViewById(R.id.btn_Force);
+        textView = (TextView) findViewById(R.id.textView);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         linearAccelerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
+        classifier = new TensorFlowClassifier(getApplicationContext());
+        count = 0;
+
+        x = new ArrayList<>();
+        y = new ArrayList<>();
+        z = new ArrayList<>();
+
         checkPermission();
         MakeChart(0, 0);
 
-        count = 0;
-
-        button.setOnClickListener(new View.OnClickListener() {
+        btn_swing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (isBtnOn == true) {
                     isBtnOn = false;
                     onPause();
-                    pitch = 0;
-                    roll = 0;
-                    yaw = 0;
-                    AngAccX = 0;
-                    AngAccY = 0;
-                    AngAccZ = 0;
                 } else if (isBtnOn == false) {
                     isBtnOn = true;
                     onResume();
-                }
-            }
-        });
-
-        btn_capture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                View rootView = getWindow().getDecorView();
-
-                File screenShot = ScreenShot(rootView);
-                if (screenShot != null) {
-                    //갤러리에 추가
-                    sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(screenShot)));
                 }
             }
         });
@@ -186,22 +157,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         btn_tensor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                TensorFlowLite();
+                DialogAnimation(P_S);
+            }
+        });
 
+        btn_Force.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "힘 계산 완료 : " + Force[50][0], Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    public void clearGraph(){
-        lineChart.invalidate();
-        lineChart.clear();
-        lineDataSet.clear();
-        lineData.clearValues();
-        count = 0;
-        inputGyro = new float[][][]{};
-        outGyro = new float[]{};
-        MakeChart(0, 0);
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // 마시멜로우 버전과 같거나 이상이라면
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                    || checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    Toast.makeText(this, "외부 저장소 사용을 위해 읽기/쓰기 필요", Toast.LENGTH_SHORT).show();
+                }
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 2);
+                //마지막 인자는 체크해야될 권한 갯수
+            } else {
+                Toast.makeText(this, "권한 승인되었음", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -218,29 +200,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-//        // 가속도 파트 , Acc
-//        if (event.sensor == linearAccelerSensor && isBtnOn == true) {
-//            //exclude gravity
-//            lAccX = event.values[0];
-//            lAccY = event.values[1];
-//            lAccZ = event.values[2];
-//
-//            tvlXaxis.setText("선형 X axis : " + String.format("%f", lAccX));
-//            tvlYaxis.setText("선형 Y axis : " + String.format("%f", lAccY));
-//            tvlZaxis.setText("선형 Z axis : " + String.format("%f", lAccZ));
-//
-//            lAcc[count] = new float[]{lAccX, lAccY, lAccZ};
-//
-//            count++;
-//            updateMarker(count, lAccX);
-//        }
-
         if (event.sensor == gyroSensor && isBtnOn == true) {
             GyroX = event.values[0];
             GyroY = event.values[1];
             GyroZ = event.values[2];
 
-            //테스트
             activityPrediction();
             x.add(event.values[0]);
             y.add(event.values[1]);
@@ -268,11 +232,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 Calculate_Force();
             }
 
-            for(int i=0; i<140; i++){
-                for(int j=0; j<3; j++){
-//                    inputGyro[0][i][j] = Gyro[i][j];
-                }
-            }
             count++;
             updateMarker(count, GyroX);
         }
@@ -280,17 +239,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     //테스트
     private void activityPrediction() {
-        if (x.size() == 140 && y.size() == 140 && z.size() == 140) {
+        if (x.size() == 150 && y.size() == 150 && z.size() == 150) {
             List<Float> data = new ArrayList<>();
             data.addAll(x);
             data.addAll(y);
             data.addAll(z);
-
-            Log.d("be_predictProbabilities","predictProbabilities_ready");
             results = classifier.predictProbabilities(toFloatArray(data));
-            Log.d("Petting", ""+ results[0]);
-            Log.d("Swing", ""+results[1]);
-            Log.d("af_predictProbabilities","predictProbabilities_complete");
+
+            textView.setText("  putting : "+results[0]+ "\n  swing : "+results[1]);
+
+            if(results[0]>results[1]){P_S = false;}
+            else if(results[0]<results[1]){P_S = true;}
 
             x.clear();
             y.clear();
@@ -298,7 +257,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
-    //테스트
     private float[] toFloatArray(List<Float> list) {
         int i = 0;
         float[] array = new float[list.size()];
@@ -319,7 +277,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //threshold 0.05 ; 의식적으로 가만히 있고자 노력할 때 각속도가 0으로 고정됨.
         //threshold 1 ; 잔류고정각속도가 0인 지점에서 0이 아닌 값이 발생하게 됨. 이는 적분시 치명적 오류야기
         if (count > 0) {
-
             if (Math.abs(GyroX - Gyro[count - 1][0]) < 0.05) {
                 GyroX = Gyro[count - 1][0];
                 Gyro[count][0] = GyroX;
@@ -330,16 +287,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 GyroZ = Gyro[count - 1][2];
                 Gyro[count][2] = GyroZ;
             }
-
         }
-
 
         //#### angle 단순 계산
         pitch += (GyroX) * (dt * Math.pow(10, -9));
         roll += (GyroY) * (dt * Math.pow(10, -9));
         yaw += (GyroZ) * (dt * Math.pow(10, -9));
         double DT = dt * Math.pow(10, -9);
-
 
         //###### Complemantary Filtering
         //가속도계로부터 얻은 값을 Arctan 적용하여 2%비율로 반영(by 누군가)
@@ -364,8 +318,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //roll이 45 dgree일때 theta, pie가 90도가 넘어가는 문제가 있다. 단순 덧셈으로 모델링 할 수 없다.
         theta = (yaw * Math.cos(roll) + pitch * Math.sin(roll));
         pie = (pitch * Math.cos(roll) + yaw * Math.sin(roll));
-//        theta = pitch;
-//        pie = yaw;
+
+//        theta = pitch; pie = yaw;
         Pos[count][0] = radios * Math.sin(theta) * Math.sin(pie);
         Pos[count][1] = radios * Math.sin(theta) * Math.cos(pie);
         Pos[count][2] = radios * Math.cos(theta);
@@ -382,9 +336,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         pie_Acc = (AngAccX * Math.cos(roll) + AngAccZ * Math.sin(roll));
 
         //토크 계산
-        Moment_Inertia = 1000;  //m단위 체계, 계산에 의하여.
+        Moment_Inertia = 0.185;  //m단위 체계, 계산에 의하여.
         Torque[count][0] = Moment_Inertia * pie_Acc;    //횡회전
         Torque[count][1] = Moment_Inertia * theta_Acc;
+
+        //타격점 힘 계산
+        totalRadios = (radios + 0.93);  //7번 club 기준, MKS 단위계 기준
+        Force[count][0] = Torque[count][0] / totalRadios;    //횡회전
+        Force[count][1] = Torque[count][1] / totalRadios;
+    }
+
+    public void clearGraph(){
+        lineChart.invalidate();
+        lineChart.clear();
+        lineDataSet.clear();
+        lineData.clearValues();
+        pitch = 0;
+        roll = 0;
+        yaw = 0;
+        AngAccX = 0;
+        AngAccY = 0;
+        AngAccZ = 0;
+        count = 0;
+        inputGyro = new float[][][]{};
+        outGyro = new float[]{};
+        textView.setText("");
+        MakeChart(0, 0);
     }
 
     List<Entry> entries = new ArrayList<>();
@@ -392,12 +369,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void MakeChart(float xValue, float yValue) {
         entries.add(new Entry(xValue, yValue));
 
-        lineDataSet = new LineDataSet(entries, "속성명1");
-        lineDataSet.setLineWidth(2);
-        lineDataSet.setCircleRadius(6);
-        lineDataSet.setCircleColor(Color.BLUE);
-        lineDataSet.setCircleColorHole(Color.BLUE);
-        lineDataSet.setColor(Color.BLUE);
+        lineDataSet = new LineDataSet(entries, "GyroX");
+        lineDataSet.setLineWidth(1);
+        lineDataSet.setCircleRadius(2);
+        lineDataSet.setCircleColor(Color.BLACK);
+        lineDataSet.setCircleColorHole(Color.BLACK);
+        lineDataSet.setColor(Color.BLACK);
         lineDataSet.setDrawCircleHole(true);
         lineDataSet.setDrawCircles(true);
         lineDataSet.setDrawHorizontalHighlightIndicator(false);
@@ -444,21 +421,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         lineChart.setMarker(marker);
     }
 
-    private void checkPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // 마시멜로우 버전과 같거나 이상이라면
-            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
-                    || checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                    Toast.makeText(this, "외부 저장소 사용을 위해 읽기/쓰기 필요", Toast.LENGTH_SHORT).show();
-                }
-                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 2);
-                //마지막 인자는 체크해야될 권한 갯수
-            } else {
-                Toast.makeText(this, "권한 승인되었음", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
     //csv또는 일반 text만들기 내부 메소드
     private boolean __makeCsvOrTxtFile(String[]... headers) {
         PrintWriter pw = null;
@@ -466,7 +428,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         try {
             pw = new PrintWriter(new File(FilePath, "Golf_" + FileSaveTime_ver + ".csv"));
 
-            String[] cellString = {"Time", "GyroX", "GyroY", "GyroZ", "PosX", "PosY", "PosZ", "TorPie", "TorThe"};
+            String[] cellString = {"Time", "GyroX", "GyroY", "GyroZ", "PosX", "PosY", "PosZ", "ForPie", "ForThe"};
 
             StringBuffer csvHeader = new StringBuffer("");
             StringBuffer csvData = new StringBuffer("");
@@ -501,69 +463,48 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     csvData.append(Pos[i][j]);
                     csvData.append(',');
                 }
-
                 if (count == 0) {
                     csvData.append('0');
                     csvData.append(',');
                     csvData.append('0');
                     csvData.append(',');
                 } else {
-                    csvData.append(Torque[i][0]);
+                    csvData.append(Force[i][0]);
                     csvData.append(',');
-                    csvData.append(Torque[i][1]);
+                    csvData.append(Force[i][1]);
                     csvData.append(',');
                 }
                 csvData.append('\n');
             }
             pw.write(csvData.toString());
             FileSaveTime_ver++;
-
             pw.close();
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        } catch (FileNotFoundException e) { e.printStackTrace();
         } finally {
-            if (pw != null) {
-                pw.close();
-            }
-            if (sb != null) {
-                sb = null;
-            }
+            if (pw != null) { pw.close(); }
+            if (sb != null) { sb = null; }
         }
         return true;
     }
 
-    //화면 캡쳐하기
-    public File ScreenShot(View view) {
-        view.setDrawingCacheEnabled(true);  //화면에 뿌릴때 캐시를 사용하게 한다
-        Bitmap screenBitmap = view.getDrawingCache();   //캐시를 비트맵으로 변환
-        File file = new File(FilePath,  "Golf_" + FileSaveTime_ver + "_img.jpg");
-        FileOutputStream os = null;
-        try {
-            os = new FileOutputStream(file);
-            screenBitmap.compress(Bitmap.CompressFormat.PNG, 90, os);   //비트맵을 PNG파일로 변환
-            os.close();
-            FileSaveTime_ver++;
-            Toast.makeText(MainActivity.this, "이미지 캡쳐", Toast.LENGTH_SHORT).show();
-            lineChart.invalidate();
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.e("IOException", "" + e);
-            return null;
-        }
-        view.setDrawingCacheEnabled(false);
-        return file;
-    }
+    // 다이어로그
+    public void DialogAnimation(Boolean P_S){
+        Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.custom_dialog);
 
-    private void TensorFlowLite() {
-//        // converted_model.tflite
-//        float[][] input = new float[140][3];
-//        float[] output = new float[2];
-//
-//        Interpreter tfile = getTfliteInterpreter("converted_model.tflite");
-//        tfile.run(Gyro, output);
-//
-//        Log.e("output", ""+output[0]);
+        ImageView image = (ImageView) dialog.findViewById(R.id.gif_image);
+        TextView textView2 = (TextView) dialog.findViewById(R.id.textView2);
+
+        GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(image);
+
+        if(P_S == false) {
+            Glide.with(this).load(R.drawable.putting).into(gifImage);
+            textView2.setText("퍼팅입니다~");
+        }
+        else if(P_S == true) {
+            Glide.with(this).load(R.drawable.swing).into(gifImage);
+            textView2.setText("스윙입니다~");
+        }
+        dialog.show();
     }
 }
